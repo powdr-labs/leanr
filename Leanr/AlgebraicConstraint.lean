77a0179d4e5f2ccaed29e49bce706f7ccd336809
chriseth <chriseth.github@gmail.com>
2025-10-01 16:14:27 +0000

    Simple solver.

--- Leanr/AlgebraicConstraint.lean
+++ Leanr/AlgebraicConstraint.lean
@@ -3,7 +3,7 @@ import Leanr.Expression
 structure AlgebraicConstraint (p : ℕ) where
   expression : Expression p
 
-def AlgebraicConstraint.is_satisfied {p : ℕ}
+def AlgebraicConstraint.is_satisfied_by {p : ℕ}
   (c : AlgebraicConstraint p)
   (env : String → ZMod p) : Prop :=
   c.expression.eval env = 0
@@ -13,3 +13,9 @@ def AlgebraicConstraint.substitute {p : ℕ}
   (x : String)
   (v : ZMod p) : AlgebraicConstraint p :=
   { expression := c.expression.substitute x v }
+
+def AlgebraicConstraint.trivial {p : ℕ}
+  (c : AlgebraicConstraint p) : Prop :=
+  match c.expression with
+  | .const c => if c.val = 0 then true else false
+  | _ => false
---
+++ Leanr/Solver.lean
@@ -0,0 +1,47 @@
+import Leanr.AlgebraicConstraint
+
+structure Assignment {p : ℕ} where
+  var : String
+  value : ZMod p
+
+--- Try to solve an affine constraint with at most one variable.
+def solve_constraint? {p : ℕ}
+  (constraint : AlgebraicConstraint p) : Option (Assignment (p := p)) :=
+  -- After simplification, we know that constants are always on the left.
+  -- So we solve expressions of the form
+  --  x
+  --  k + x
+  --  c * x
+  --  k + c * x
+  match constraint.expression with
+  | .var x => some { var := x, value := 0 }
+  | .add (.const k) (.var x) => some { var := x, value := -k }
+  | .mul (.const c) (.var x) =>
+    if c = 0 then none else some { var := x, value := c⁻¹ * 0 }
+  | .add (.const k) (.mul (.const c) (.var x)) =>
+    if c = 0 then none else some { var := x, value := c⁻¹ * -k }
+  | _ => none
+
+-- TODO theorem: solve plus substitute yields trivial constraint.
+
+theorem solve_constraint?_correct {p : ℕ}
+  (constraint : AlgebraicConstraint p)
+  (assignment : Assignment (p := p))
+  (h : solve_constraint? constraint = some assignment)
+  (env : String → ZMod p)
+  (satisfying : constraint.is_satisfied_by env) :
+  env assignment.var = assignment.value := by
+  sorry
+
+def find_assignments {p : ℕ}
+  (constraints : List (AlgebraicConstraint p)) : List (Assignment (p := p)) :=
+  constraints.filterMap solve_constraint?
+
+def find_and_apply_assignments {p : ℕ}
+  (constraints : List (AlgebraicConstraint p)) :
+  List (AlgebraicConstraint p) :=
+  let assignments := find_assignments constraints
+  constraints.map (fun c =>
+    assignments.foldl (fun acc a => acc.substitute a.var a.value) c)
+  -- new_constraints.filterMap fun c =>
+  --   if c.trivial then none else some c
