24b875c1cd63f5ef8882ef46505990d4019f8c5e
chriseth <chriseth.github@gmail.com>
2025-10-02 13:20:01 +0000

    Simple parser.

---
+++ Leanr/Parser.lean
@@ -0,0 +1,32 @@
+import Init.Data.ToString
+
+import Leanr.Expression
+
+declare_syntax_cat expr
+
+syntax:65 expr:65  "+" expr:66 : expr
+syntax:70 expr:70 "*" expr:71 : expr
+syntax "(" expr ")" : expr
+syntax num : expr
+syntax ident : expr
+
+syntax "expr" "{" expr "}" : term
+
+macro_rules
+  | `(expr {$x + $y}) => `(Expression.simplifying_add (expr {$x}) (expr {$y}))
+  | `(expr {$x * $y}) => `(Expression.simplifying_mul (expr {$x}) (expr {$y}))
+  | `(expr {($x)}) => `(expr {$x})
+  | `(expr {$n:num}) => `(.const (OfNat.ofNat $n))
+  | `(expr {$x:ident}) => `(.var $(Lean.quote x.getId.toString))
+
+/-- info: (2 * x) -/
+#guard_msgs in
+#eval (expr { 2 * x + 0 } : Expression 9)
+
+/-- info: (4 + ((2 * x) + (3 * y))) -/
+#guard_msgs in
+#eval ((expr { 2 * x + 3 * y + 4 }) : Expression 9)
+
+/-- info: ((2 * x) + (3 * y)) -/
+#guard_msgs in
+#eval ((expr { 2 * x + 3 * y + 4 }) : Expression 4)
