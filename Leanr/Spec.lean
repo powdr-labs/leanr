822ac2ee95331a326762fb82929b05b0997debcf
Georg Wiese <georgwiese@gmail.com>
2026-07-01 16:12:54 +0200

    Distinguish between constraint violation and invariant violation

--- Leanr/Spec.lean
+++ Leanr/Spec.lean
@@ -46,12 +46,14 @@ structure BusSemantics (p : ℕ) where
   /-- Whether the bus of the given ID changes the state of the VM.
       Stateless bus interactions are typically lookups. -/
   isStateful (busId : Nat) : Bool
-  /-- Whether the bus interaction message is accepted by the rest of the system.
-      If this returns `false`, it *must* be that there is a constraint violation
-      somewhere in the zkVM that caused the message to be rejected.
-      For example, if the bus implements a lookup, this function would check
-      whether the bus interaction message exists in the lookup table. -/
-  accepts (busInteractionMessage : BusInteraction (ZMod p)) : Bool
+  /-- Whether sending this bus interaction message violates a constraint in *another* chip.
+      An example of this is sending a message that conflicts with a lookup table entry. -/
+  violatesConstraint (busInteractionMessage : BusInteraction (ZMod p)) : Bool
+  /-- Whether sending this bus interaction message breaks an invariant on which soundness
+      of the system depends.
+      For example, a memory bus might have the invariant that all sent values must be in
+      a certain range. -/
+  breaksInvariant (busInteractionMessage : BusInteraction (ZMod p)) : Bool
 
 /-- A concrete bus interaction message: which bus, and the tuple sent. -/
 abbrev BusMessage (p : ℕ) := Nat × List (ZMod p)
@@ -80,15 +82,22 @@ noncomputable def ConstraintSystem.sideEffects (cs : ConstraintSystem p)
 def ConstraintSystem.satisfies (s : ConstraintSystem p) (busSemantics : BusSemantics p)
     (env : String → ZMod p) : Prop :=
   (∀ c ∈ s.algebraicConstraints, c.eval env = 0) ∧
-  (∀ bi ∈ s.busInteractions, busSemantics.accepts (bi.eval env))
+  (∀ bi ∈ s.busInteractions,
+    let message := bi.eval env
+    message.multiplicity ≠ 0 → busSemantics.violatesConstraint message = false)
+
+/-- Whether a constraint system guarantees that all invariants are maintained under a given bus semantics. -/
+def ConstraintSystem.guaranteesInvariants (s : ConstraintSystem p) (busSemantics : BusSemantics p) : Prop :=
+  ∀ env, s.satisfies busSemantics env → ∀ bi ∈ s.busInteractions,
+    let message := bi.eval env
+    message.multiplicity ≠ 0 → busSemantics.breaksInvariant message = false
 
 /-- Whether a constraint system implies another under a given bus semantics.
     Informally, a constraint system `self` implies a system `other` if for every
     satisfying assignment of `self`:
     1. There exists a satisfying assignment of `other`
     2. The side effects of `self` under the given environment and bus semantics
-       are equal to the side effects of `other` under the corresponding environment.
--/
+       are equal to the side effects of `other` under the corresponding environment. -/
 def ConstraintSystem.implies (self other : ConstraintSystem p) (busSemantics : BusSemantics p) :
     Prop :=
   ∀ env, self.satisfies busSemantics env →
@@ -101,10 +110,14 @@ def ConstraintSystem.equivalentTo (self other : ConstraintSystem p) (busSemantic
     Prop :=
   self.implies other busSemantics ∧ other.implies self busSemantics
 
-/-- Whether an optimizer maintains circuit equivalence.
-    An optimizer maintains circuit equivalence if, for every constraint system and bus semantics,
-    the optimized constraint system is equivalent to the original. -/
-def optimizerMaintainsCircuitEquivalence (optimizer : ConstraintSystem p → BusSemantics p → ConstraintSystem p) :
+/-- Whether an optimizer maintains correctness. This means that, for all constraint systems
+    and bus semantics:
+    1. The optimized constraint system is equivalent to the original, i.e. it has a satisfying
+       witness iff the original does.
+    2. Assuming the original constraint system guarantees invariants, so does the optimized one. -/
+def optimizerMaintainsCorrectness (optimizer : ConstraintSystem p → BusSemantics p → ConstraintSystem p) :
     Prop :=
   ∀ constraintSystem busSemantics,
-    (optimizer constraintSystem busSemantics).equivalentTo constraintSystem busSemantics
+    ((optimizer constraintSystem busSemantics).equivalentTo constraintSystem busSemantics) ∧
+    (constraintSystem.guaranteesInvariants busSemantics →
+      (optimizer constraintSystem busSemantics).guaranteesInvariants busSemantics)
