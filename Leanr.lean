be160eaf96f7675bc7de737045ac0bc0cb5feade
Georg Wiese <georgwiese@gmail.com>
2026-07-01 20:29:35 +0200

    Add OpenVM bus semantics

--- Leanr.lean
+++ Leanr.lean
@@ -12,6 +12,7 @@ import Leanr.IdentityOptimizer
 import Leanr.StringParser
 import Leanr.JsonParser
 import Leanr.OpenVM
+import Leanr.OpenVM.Semantics
 import Leanr.RangeConstraint
 import Leanr.RangeConstraint.Proofs
 import Leanr.Expression.Proofs
