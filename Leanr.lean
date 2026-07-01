36001bea75d7995e6ec1345873826766a4a753b3
Georg Wiese <georgwiese@gmail.com>
2026-07-01 21:21:36 +0200

    Remove most of the codebase

--- Leanr.lean
+++ Leanr.lean
@@ -1,24 +1,8 @@
 -- This module serves as the root of the `Leanr` library.
 -- Import modules here that should be built as part of the library.
-import Leanr.Basic
-import Leanr.Expression
-import Leanr.AffineExpression
-import Leanr.AlgebraicConstraint
-import Leanr.BusInteraction
-import Leanr.System
-import Leanr.Solver
 import Leanr.Spec
 import Leanr.Utils.Dsl
 import Leanr.Utils.Size
-import Leanr.IdentityOptimizer
-import Leanr.StringParser
-import Leanr.JsonParser
-import Leanr.OpenVM
+import Leanr.Optimizer
 import Leanr.OpenVM.Semantics
 import Leanr.OpenVM.Snapshot
-import Leanr.RangeConstraint
-import Leanr.RangeConstraint.Proofs
-import Leanr.Expression.Proofs
-import Leanr.AffineExpression.Proofs
-import Leanr.AlgebraicConstraint.Proofs
-import Leanr.Solver.Proofs
